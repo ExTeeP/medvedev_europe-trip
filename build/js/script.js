b64c5695b577651a09066fcc16de04049236b9a6
ExTeeP <exteep@yandex.ru>
2021-04-29 15:51:50 +0300

    Разметка и стили футера и хедера
    подключение шрифтов и небольшая разметка других блоков

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,68 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+(function () {
+  var throttle = function (type, name, obj) {
+    obj = obj || window;
+    var running = false;
 
-pageHeader.classList.remove('page-header--nojs');
+    var func = function () {
+      if (running) {
+        return;
+      }
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+      running = true;
+
+      requestAnimationFrame(function () {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+
+    obj.addEventListener(type, func);
+  };
+
+  throttle('resize', 'optimizedResize');
+})();
+
+var MAX_WIDTH = 1023.8;
+
+var toggle = document.querySelector('.burger');
+var menu = document.querySelector('.header__menu');
+
+var changeButtonLabel = function () {
+  if (toggle.classList.contains('burger__active')) {
+    toggle.children[0].innerText = 'Закрыть меню';
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    toggle.children[0].innerText = 'Открыть меню';
+  }
+};
+
+var showerMenu = function () {
+  document.documentElement.classList.toggle('page--menu-open');
+  toggle.classList.toggle('burger__active');
+  menu.classList.toggle('header__menu-open');
+
+  changeButtonLabel();
+};
+
+var onMenuLinkClick = function (evt) {
+  if (evt.target.tagName === 'A') {
+    showerMenu();
+  }
+};
+
+var onMenuButtonClick = function () {
+  showerMenu();
+};
+
+toggle.addEventListener('click', onMenuButtonClick);
+menu.addEventListener('click', onMenuLinkClick);
+
+window.addEventListener('optimizedResize', function () {
+  if (window.innerWidth > MAX_WIDTH) {
+    toggle.removeEventListener('click', onMenuButtonClick);
+    menu.removeEventListener('click', onMenuLinkClick);
+  } else {
+    toggle.addEventListener('click', onMenuButtonClick);
+    menu.addEventListener('click', onMenuLinkClick);
   }
 });
